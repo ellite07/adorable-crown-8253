862ab62880c16d43daddabcd6d83fc1ba96462cc
Tushar Shelke <97495058+ellite07@users.noreply.github.com>
2022-07-21 17:45:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Customers from "./pages/Customers"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Customers/>
+      <h1>app</h1>
     </div>
   )
 }
